b714aebb98a3e292ac2fc35009379c670c1a244a
kajaljagtap2005 <kajaljagtapofficial@gmail.com>
2025-03-29 22:47:30 +0530

    Delete web project directory

--- web project/script.js
+++
@@ -1,8 +0,0 @@
- // Add this to initialize the circular progress bars
-document.addEventListener('DOMContentLoaded', function() {
-    const circles = document.querySelectorAll('.circle-progress');
-    circles.forEach(circle => {
-        const percent = circle.querySelector('.number').textContent;
-        circle.style.setProperty('--target-percent', percent);
-    });
-});
